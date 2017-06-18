88911d0f68d694b4828f5ebf9a9741df9a354f15
talvin3n <solamente12dios11sabe@gmail.com>
2017-06-18 14:28:23 +0900

    Update hello.asm

--- linux/x86_64/hello.asm
+++ linux/x86_64/hello.asm
@@ -1,4 +1,4 @@
-; write(1, "Hello, world!!\r\n", 0xf)
+; write(1, "Hello, world!!\r\n", 0x10)
 xor    rax, rax
 push    rax
 mov    r8, 0x0a0d2121646c726f
@@ -6,11 +6,11 @@ push    r8
 mov    r8, 0x77202c6f6c6c6548
 push    r8
 xor    rdi, rdi
-inc    al
 inc    dil
 mov    rsi, rsp
 xor    rdx, rdx
 mov    dl, 0x10
+inc    al
 syscall
 
 ; exit(0)
